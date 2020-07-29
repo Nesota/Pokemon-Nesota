e62e2083c7c71fe27e763d20b3f2a317f6f4d66d
Mike_Keller <mkeller1992@gmail.com>
2020-07-28 21:18:54 -0500

    Initial Commit

---
+++ data/pokemon/base_stats/mootle.asm
@@ -0,0 +1,22 @@
+	db MOOTLE ; 252	
+
+	db  55, 60,  75, 80,  30,  60	
+	;   hp  atk  def  spd  sat  sdf	
+
+	db NORMAL, NORMAL ; type	
+	db 175 ; catch rate	
+	db 100 ; base exp	
+	db NO_ITEM, NO_ITEM ; items	
+	db GENDER_F50 ; gender ratio	
+	db 100 ; unknown 1	
+	db 20 ; step cycles to hatch	
+	db 5 ; unknown 2	
+	INCBIN "gfx/pokemon/mootle/front.dimensions"	
+	dw NULL, NULL ; unused (beta front/back pics)	
+	db GROWTH_SLOW ; growth rate	
+	dn EGG_NONE ; egg groups	
+
+	; tm/hm learnset	
+	tmhm HEADBUTT, CURSE, TOXIC, ZAP_CANNON, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, EARTHQUAKE, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, REST, ATTRACT, SURF, STRENGTH, FLAMETHROWER, THUNDERBOLT, ICE_BEAM	
+	; end
+	
---
+++ data/pokemon/dex_entries/mootle.asm
@@ -0,0 +1,10 @@
+	db "BABY COW@" ; species name	
+	dw 209, 960 ; height, weight	
+
+	db   "This newly found"	
+	next "'Mon, is thought"	
+	next "to be the"	
+
+	page "offspring of a"	
+	next "Miltank and a"	
+	next "Tauros.@"
